222c4e122df9f4a20fadec0937788cbd74e2febf
EwingTsai <191237878@qq.com>
2017-12-07 01:44:52 +0800

    更新实体模型

--- src/main/resources/firstdata.sql
+++ src/main/resources/firstdata.sql
@@ -4,43 +4,43 @@ Target Server Version : 50718
 File Encoding         : 65001
 */
 
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for permission
 -- ----------------------------
 DROP TABLE IF EXISTS `permission`;
 CREATE TABLE `permission` (
-  `id`          BIGINT (20) NOT NULL AUTO_INCREMENT,
-  `parent_id`   BIGINT (20) DEFAULT NULL,
-  `name`        VARCHAR(64) NOT NULL,
-  `code`        VARCHAR(64) NOT NULL,
-  `type`        VARCHAR(64)   DEFAULT NULL,
-  `target`      VARCHAR(1024) DEFAULT NULL,
-  `create_time` DATETIME      DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL,
+  `name` varchar(128) NOT NULL,
+  `code` varchar(64) NOT NULL,
+  `type` varchar(16) NOT NULL,
+  `target` varchar(512) DEFAULT NULL,
+  `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_permission_parent_id` (`parent_id`),
   CONSTRAINT `fk_permission_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `permission` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE =InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of permission
 -- ----------------------------
-INSERT INTO `permission` VALUES ('1', NULL, '新增用户', 'USER_ADD', '1', NULL, '2017-08-24 12:09:10');
-INSERT INTO `permission` VALUES ('2', NULL, '删除用户', 'USER_DELETE', '1', NULL, '2017-08-24 12:09:52');
-INSERT INTO `permission` VALUES ('3', NULL, '查看用户', 'USER_VIEW', '1', NULL, '2017-08-24 12:10:24');
+INSERT INTO `permission` VALUES ('1', null, '新增用户', 'USER_ADD', 'OPERATION', null, '2017-08-24 12:09:10');
+INSERT INTO `permission` VALUES ('2', null, '删除用户', 'USER_DELETE', 'OPERATION', null, '2017-08-24 12:09:52');
+INSERT INTO `permission` VALUES ('3', null, '查看用户', 'USER_VIEW', 'PAGE', null, '2017-08-24 12:10:24');
 
 -- ----------------------------
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
-  `id`          BIGINT (20) NOT NULL AUTO_INCREMENT,
-  `name`        VARCHAR(64) DEFAULT NULL,
-  `code`        VARCHAR(64) NOT NULL,
-  `create_time` DATETIME    DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `code` varchar(64) NOT NULL,
+  `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE =InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of role
@@ -52,15 +52,15 @@ INSERT INTO `role` VALUES ('1', '用户管理', 'ROLE_USER', '2017-08-24 12:07:5
 -- ----------------------------
 DROP TABLE IF EXISTS `role_permission`;
 CREATE TABLE `role_permission` (
-  `role_id`       BIGINT (20) NOT NULL,
-  `permission_id` BIGINT (20) NOT NULL,
-  `create_time`   DATETIME DEFAULT NULL,
-  PRIMARY KEY (`role_id`, `permission_id`),
+  `role_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`role_id`,`permission_id`),
   KEY `fk_permission_role` (`permission_id`),
   KEY `fk_role_permission` (`role_id`),
   CONSTRAINT `fk_permission_role` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_role_permission` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE =InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of role_permission
@@ -74,34 +74,35 @@ INSERT INTO `role_permission` VALUES ('1', '3', '2017-08-24 12:11:50');
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id`          BIGINT (20) NOT NULL AUTO_INCREMENT,
-  `name`        VARCHAR(64) NOT NULL,
-  `password`    VARCHAR(32) NOT NULL,
-  `birthday`    DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `create_time` DATETIME DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `gender` varchar(16) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE =InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '元宝', 'yb', '2000-02-10 12:22:30', '2017-08-23 18:43:52');
-INSERT INTO `user` VALUES ('2', '露娜', 'ln', '2002-05-20 12:22:46', '2017-08-24 12:06:02');
+INSERT INTO `user` VALUES ('1', '元宝', 'yb', 'MALE', '2000-02-10', '2017-08-23 18:43:52');
+INSERT INTO `user` VALUES ('2', '露娜', 'ln', 'FEMALE', '2002-05-20', '2017-08-24 12:06:02');
 
 -- ----------------------------
 -- Table structure for user_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `user_permission`;
 CREATE TABLE `user_permission` (
-  `user_id`       BIGINT (20) NOT NULL,
-  `permission_id` BIGINT (20) NOT NULL,
-  `create_time`   DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`, `permission_id`),
+  `user_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL,
+  `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`,`permission_id`),
   KEY `fk_permission_user` (`permission_id`),
   KEY `fk_user_permission` (`user_id`),
   CONSTRAINT `fk_permission_user` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_user_permission` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE =InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_permission
@@ -113,15 +114,15 @@ INSERT INTO `user_permission` VALUES ('2', '3', '2017-08-24 12:12:15');
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
-  `role_id`     BIGINT (20) NOT NULL,
-  `user_id`     BIGINT (20) NOT NULL,
-  `create_time` DATETIME DEFAULT NULL,
-  PRIMARY KEY (`role_id`, `user_id`),
+  `role_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`role_id`,`user_id`),
   KEY `fk_role_user` (`role_id`),
   KEY `fk_user_role` (`user_id`),
   CONSTRAINT `fk_role_user` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_user_role` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE =InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_role
