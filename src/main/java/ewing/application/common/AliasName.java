a2d698882614f817cc64e0ef311a6fd5264b55fc
EwingTsai <191237878@qq.com>
2017-12-08 11:30:23 +0800

    支持属性序列化时添加别名

--- src/main/java/ewing/application/common/AliasName.java
+++ src/main/java/ewing/application/common/AliasName.java
@@ -1,11 +1,13 @@
 package ewing.application.common;
 
+import java.io.Serializable;
+
 /**
  * 有别名的接口。
  */
 public interface AliasName {
 
-    String name();
+    Serializable name();
 
     String alias();
 
