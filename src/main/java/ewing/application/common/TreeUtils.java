16bfb394212fe37397725cf8a3a40b0ce77f3d73
EwingTsai <191237878@qq.com>
2017-12-13 00:17:58 +0800

    添加树的遍历

--- src/main/java/ewing/application/common/TreeUtils.java
+++ src/main/java/ewing/application/common/TreeUtils.java
@@ -1,8 +1,11 @@
 package ewing.application.common;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * 树工具类。
@@ -13,13 +16,10 @@ public class TreeUtils {
      * 树节点集合转换为树形结构。
      * 有父节点的挂在父节点下，未找到父结节的置于顶级。
      */
-    public static <E extends TreeNode> List<E> toTree(Collection<E> nodes) {
+    @SuppressWarnings("unchecked")
+    public static <E extends TreeNode> List<E> toTree(List<E> nodes) {
         if (nodes == null) {
             return null;
-        }
-        // 清理之前构建的树，只支持节点为单节点
-        for (E node : nodes) {
-            node.setChildren(null);
         }
         List<E> tree = new ArrayList<>();
         boolean single;
@@ -47,6 +47,102 @@ public class TreeUtils {
             }
         }
         return tree;
+    }
+
+    /**
+     * 先根遍历原子节点，无递归，支持大树。
+     */
+    @SuppressWarnings("unchecked")
+    public static <E extends TreeNode> void traverseTree(List<E> tree, Consumer<E> consumer) {
+        // 使用迭代器和栈记录所有遍历状态
+        Stack<Iterator<E>> stack = new Stack<>();
+        stack.push(tree.iterator());
+        while (!stack.isEmpty()) {
+            Iterator<E> iterator = stack.pop();
+            while (iterator.hasNext()) {
+                E node = iterator.next();
+                if (node.getChildren() != null) {
+                    stack.push(iterator);
+                    iterator = node.getChildren().iterator();
+                }
+                consumer.accept(node);
+            }
+        }
+    }
+
+    /**
+     * 把树的所有节点放入列表，无递归，支持大树。
+     */
+    @SuppressWarnings("unchecked")
+    public static <E extends TreeNode> List<E> toList(List<E> tree) {
+        List<E> nodes = new ArrayList<>();
+        // 使用迭代器和栈记录所有遍历状态
+        Stack<Iterator<E>> stack = new Stack<>();
+        stack.push(tree.iterator());
+        while (!stack.isEmpty()) {
+            Iterator<E> iterator = stack.pop();
+            while (iterator.hasNext()) {
+                // 先遍历自己，然后遍历子节点
+                E node = iterator.next();
+                if (node.getChildren() != null) {
+                    stack.push(iterator);
+                    iterator = node.getChildren().iterator();
+                }
+                nodes.add(node);
+            }
+        }
+        return nodes;
+    }
+
+    /**
+     * 从树的所有节点中查找原子节点，无递归，支持大树。
+     */
+    @SuppressWarnings("unchecked")
+    public static <E extends TreeNode> List<E> filterTree(List<E> tree, Predicate<E> predicate) {
+        List<E> nodes = new ArrayList<>();
+        // 使用迭代器和栈记录所有遍历状态
+        Stack<Iterator<E>> stack = new Stack<>();
+        stack.push(tree.iterator());
+        while (!stack.isEmpty()) {
+            Iterator<E> iterator = stack.pop();
+            while (iterator.hasNext()) {
+                // 先遍历自己，然后遍历子节点
+                E node = iterator.next();
+                if (node.getChildren() != null) {
+                    stack.push(iterator);
+                    iterator = node.getChildren().iterator();
+                }
+                if (predicate.test(node)) {
+                    nodes.add(node);
+                }
+            }
+        }
+        return nodes;
+    }
+
+    /**
+     * 从树的所有节点中查找第一个原子节点，无递归，支持大树。
+     */
+    @SuppressWarnings("unchecked")
+    public static <E extends TreeNode> E findFirst(List<E> tree, Predicate<E> predicate) {
+        // 使用迭代器和栈记录所有遍历状态
+        Stack<Iterator<E>> stack = new Stack<>();
+        stack.push(tree.iterator());
+        while (!stack.isEmpty()) {
+            Iterator<E> iterator = stack.pop();
+            while (iterator.hasNext()) {
+                // 先遍历自己，然后遍历子节点
+                E node = iterator.next();
+                if (predicate.test(node)) {
+                    return node;
+                }
+                if (node.getChildren() != null) {
+                    stack.push(iterator);
+                    iterator = node.getChildren().iterator();
+                }
+            }
+        }
+        return null;
     }
 
 }
